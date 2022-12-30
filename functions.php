a2cedc2c4fb8756b2ef7846fadb4ef83868d8f98
Chris Baldelomar <chris@webplant.media>
2022-12-30 09:45:45 -0600

    update

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function gallop_reader_enqueue_fonts()
 	// wp_enqueue_style('gallop-theme-google-fonts', 'https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap', array(), null); // null allows google fonts to have multiple family args in url
 	// wp_enqueue_style('gallop-theme-custom-fonts', 'https://use.typekit.net/zho6vbm.css', array(), '1.2');
 
-	wp_enqueue_style('gallop-reader-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.17');
+	wp_enqueue_style('gallop-reader-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.18');
 }
 
 function gallop_reader_support()
@@ -20,16 +20,7 @@ function gallop_reader_support()
 
 add_action('after_setup_theme', 'gallop_reader_support');
 
-function my_wpdiscuz_shortcode()
-{
-	if (file_exists(ABSPATH . 'wp-content/plugins/wpdiscuz/templates/comment/comment-form.php')) {
-		ob_start();
-		include_once ABSPATH . 'wp-content/plugins/wpdiscuz/templates/comment/comment-form.php';
-		return ob_get_clean();
-	}
-}
-add_action('init', 'gallop_init');
-
+// add_action('init', 'gallop_init');
 function gallop_init()
 {
 	add_action('cld_after_ajax_process', 'gallop_comment_like_email', 10, 1);
@@ -135,4 +126,65 @@ function gallop_reader_comment_form_defaults($defaults)
 
 
 	return $defaults;
+}
+
+add_filter('block_type_metadata_settings', 'gallop_reader_block_type_metadata_settings', 10, 2);
+function gallop_reader_block_type_metadata_settings($array, $metadata)
+{
+	if (isset($metadata['name']) && $metadata['name'] === 'core/post-comments-link') {
+		$array['render_callback'] = 'gallop_reader_render_block_core_post_comments_link';
+	}
+
+	return $array;
+}
+function gallop_reader_render_block_core_post_comments_link($attributes, $content, $block)
+{
+	if (
+		!isset($block->context['postId']) ||
+		isset($block->context['postId']) &&
+		!comments_open($block->context['postId'])
+	) {
+		return '';
+	}
+
+	$align_class_name   = empty($attributes['textAlign']) ? '' : "has-text-align-{$attributes['textAlign']}";
+	$wrapper_attributes = get_block_wrapper_attributes(array('class' => $align_class_name));
+	$comments_number    = (int) get_comments_number($block->context['postId']);
+	$comments_link      = get_comments_link($block->context['postId']);
+	$post_title         = get_the_title($block->context['postId']);
+	$comment_html       = '';
+
+	if (0 === $comments_number) {
+		$comment_html = sprintf(
+			/* translators: %s post title */
+			__('No comments<span class="screen-reader-text"> on %s</span>'),
+			$post_title
+		);
+	} else {
+		$comment_html = sprintf(
+			/* translators: 1: Number of comments, 2: post title */
+			_n(
+				'%1$s comment<span class="screen-reader-text"> on %2$s</span>',
+				'%1$s comments<span class="screen-reader-text"> on %2$s</span>',
+				$comments_number
+			),
+			esc_html(number_format_i18n($comments_number)),
+			$post_title
+		);
+	}
+
+	$post_count = intval(get_post_meta($block->context['postId'], 'pld_like_count', true));
+
+	global $wpdb;
+	$querystr = "SELECT SUM($wpdb->commentmeta.meta_value) AS total_comment_count FROM $wpdb->comments JOIN $wpdb->commentmeta ON $wpdb->comments.comment_ID = $wpdb->commentmeta.comment_ID WHERE $wpdb->comments.comment_post_ID = " . $block->context['postId'] . " AND $wpdb->commentmeta.meta_key = 'cld_like_count'";
+	$total_comment_count =  $wpdb->get_var($querystr);
+	$total_count = $post_count + $total_comment_count;
+
+	$post_likes = '';
+	if ($total_count > 0) {
+		$post_likes = '<div class="post-likes-sum"><i class="fas fa-thumbs-up"></i>&nbsp;' . $total_count . '</div>';
+		// $post_likes = '<div class="post-likes-sum">' . $total_count . ' ' . ($total_count === 1 ? 'like' : 'likes') . '</div>';
+	}
+
+	return '<div ' . $wrapper_attributes . '><a href=' . esc_url($comments_link) . '>' . $comment_html . '</a></div>' . $post_likes;
 }
